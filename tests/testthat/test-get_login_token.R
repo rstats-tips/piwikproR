763e9eee311cce4fae5d316b2335f70db91dbf5c
Martin Stingl <martin.stingl@dfv.de>
2021-05-14 19:04:10 +0200

    Skipping tests on CRAN

--- tests/testthat/test-get_login_token.R
+++ tests/testthat/test-get_login_token.R
@@ -2,6 +2,7 @@ test_that("get_login_token works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
+  skip_on_cran()
 
   piwikproRTests::get_login_token_works(piwik_pro_credentials)
 })
--- tests/testthat/test-send_query.R
+++ tests/testthat/test-send_query.R
@@ -2,7 +2,8 @@ test_that("simple request works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::simple_request_works(piwik_pro_credentials)
 })
 
@@ -10,7 +11,8 @@ test_that("metric filter works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::metric_filter_works(piwik_pro_credentials)
 })
 
@@ -18,7 +20,8 @@ test_that("dimension filter works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::metric_filter_works(piwik_pro_credentials)
 })
 
@@ -27,7 +30,8 @@ test_that("csv data retrieval works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::csv_data_retrieval_works(piwik_pro_credentials)
 })
 
@@ -36,7 +40,8 @@ test_that("NULL values at level 3 works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::NULL_values_at_level_3_works(piwik_pro_credentials)
 })
 
@@ -45,7 +50,8 @@ test_that("NULL values at level 2 works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::NULL_values_at_level_2_works(piwik_pro_credentials)
 })
 
@@ -53,7 +59,8 @@ test_that("umlauts works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::umlauts_works(piwik_pro_credentials)
 })
 
@@ -61,7 +68,8 @@ test_that("list values as return value works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::list_values_as_return_value_works(piwik_pro_credentials)
 })
 
@@ -69,7 +77,8 @@ test_that("filter with transformation works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::filter_with_transformation_works(piwik_pro_credentials)
 })
 
@@ -77,7 +86,8 @@ test_that("filter with and without transformation works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::filter_with_and_without_transformation_works(piwik_pro_credentials)
 })
 
@@ -85,7 +95,8 @@ test_that("filter with different type for value using lists works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::filter_with_different_type_for_value_using_lists_works(piwik_pro_credentials)
 })
 
@@ -93,7 +104,8 @@ test_that("daily splitting with empty results works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::daily_splitting_with_empty_results_works(piwik_pro_credentials)
 })
 
@@ -101,7 +113,8 @@ test_that("paging works", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::paging_works(piwik_pro_credentials)
 })
 
@@ -109,6 +122,7 @@ test_that("types are set for empty results", {
   piwik_pro_credentials <- get_test_credentials()
   skip_if_missing_credentials(piwik_pro_credentials)
   skip_if_not_installed("piwikproRTests")
-
+  skip_on_cran()
+  
   piwikproRTests::types_are_set_for_empty_results(piwik_pro_credentials)
 })
