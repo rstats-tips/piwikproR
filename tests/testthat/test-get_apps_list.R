3c8d006e31850d725791b9bda46c0d1005212b9e
Martin Stingl <martin.stingl@dfv.de>
2022-02-11 14:59:49 +0100

    Permissions, Meta-Sites, Usergroup-Members

--- tests/testthat/test-get_apps_list.R
+++ tests/testthat/test-get_apps_list.R
@@ -6,3 +6,30 @@ test_that("fetching apps works", {
 
   piwikproRTests::fetching_apps_works(piwik_pro_credentials)
 })
+
+test_that("fetching metasites works", {
+  piwik_pro_credentials <- get_test_credentials()
+  skip_if_missing_credentials(piwik_pro_credentials)
+  skip_if_not_installed("piwikproRTests")
+  skip_on_cran()
+
+  piwikproRTests::fetching_metasites_works(piwik_pro_credentials)
+})
+
+test_that("fetching permissions for apps works", {
+  piwik_pro_credentials <- get_test_credentials()
+  skip_if_missing_credentials(piwik_pro_credentials)
+  skip_if_not_installed("piwikproRTests")
+  skip_on_cran()
+
+  piwikproRTests::fetching_permissions_for_apps_works(piwik_pro_credentials)
+})
+
+test_that("fetching permissions for metasites works", {
+  piwik_pro_credentials <- get_test_credentials()
+  skip_if_missing_credentials(piwik_pro_credentials)
+  skip_if_not_installed("piwikproRTests")
+  skip_on_cran()
+
+  piwikproRTests::fetching_permissions_for_metasites_works(piwik_pro_credentials)
+})
